387cf85aff61ba811543fd17bc1d9faed40bd2db
brislazaro <brisa.lazaroc@gmail.com>
2023-09-20 13:01:13 +0200

    create products cards and cart

--- scripts/home.js
+++ scripts/home.js
@@ -31,23 +31,83 @@ const products = [
 
 const productsListEl = document.querySelector(".products");
 
-const currentProduct = products[0];
+for (let i = 0; i < products.length; i++) {
+  const currentProduct = products[i];
 
-// creamos los el
-const productItemEl = document.createElement("li");
-productItemEl.classList.add("products__item", "products__item--product");
+  // creamos los el
+  const productItemEl = document.createElement("li");
+  productItemEl.classList.add("products__item", "products__item--product");
 
-const productNameEl = document.createElement("p");
-productNameEl.classList.add("item__name");
-productNameEl.innerHTML = currentProduct.name;
+  const productNameEl = document.createElement("p");
+  productNameEl.classList.add("item__name");
+  productNameEl.innerHTML = currentProduct.name;
 
-const productButtonEl = document.createElement("button");
-productButtonEl.classList.add("item__button");
-productButtonEl.innerHTML = "+1";
+  const productButtonEl = document.createElement("button");
+  productButtonEl.classList.add("item__button");
+  productButtonEl.innerHTML = "+1";
 
-// metemos el dentro del product item
-productItemEl.appendChild(productNameEl);
-productItemEl.appendChild(productButtonEl);
+  // metemos el dentro del product item
+  productItemEl.appendChild(productNameEl);
+  productItemEl.appendChild(productButtonEl);
 
-// metemos el product item en la lista de productos
-productsListEl.appendChild(productItemEl);
+  // metemos el product item en la lista de productos
+  productsListEl.appendChild(productItemEl);
+}
+
+const cart = [
+  {
+    name: "Zanahorias",
+    price: 2,
+    quantity: 3,
+  },
+  {
+    name: "Tomates",
+    price: 0.5,
+    quantity: 1,
+  },
+  {
+    name: "Huevos",
+    price: 1,
+    quantity: 2,
+  },
+];
+
+const cartListEl = document.querySelector(".cart");
+
+for (let i = 0; i < cart.length; i++) {
+  const currentItem = cart[i];
+
+  const itemCart = document.createElement("li");
+  itemCart.classList.add("cart__item");
+
+  const itemName = document.createElement("p");
+  itemName.classList.add("item__name");
+  itemName.innerHTML = currentItem.name;
+
+  itemCart.appendChild(itemName);
+
+  const itemQuantity = document.createElement("p");
+  itemQuantity.classList.add("item__quantity");
+  itemQuantity.innerHTML = currentItem.quantity;
+
+  itemCart.appendChild(itemQuantity);
+
+  const buttonItem = document.createElement("div");
+  buttonItem.classList.add("item__buttons");
+
+  itemCart.appendChild(buttonItem);
+
+  const itemButtonLess = document.createElement("button");
+  itemButtonLess.classList.add("item__button");
+  itemButtonLess.innerHTML = "-1";
+
+  buttonItem.appendChild(itemButtonLess);
+
+  const itemButtonPlus = document.createElement("button");
+  itemButtonPlus.classList.add("item__button");
+  itemButtonPlus.innerHTML = "+1";
+
+  buttonItem.appendChild(itemButtonPlus);
+
+  cartListEl.appendChild(itemCart);
+}
