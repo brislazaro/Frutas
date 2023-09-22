9bfc95c7169b9cf856b9c31e50c4b3a4371d7a01
brislazaro <brisa.lazaroc@gmail.com>
2023-09-22 13:00:17 +0200

    fix the button plus in the inventory

--- scripts/home.js
+++ scripts/home.js
@@ -46,6 +46,25 @@ for (let i = 0; i < products.length; i++) {
   productButtonEl.classList.add("item__button");
   productButtonEl.innerHTML = "+1";
 
+  productButtonEl.addEventListener("click", () => {
+    const foundProduct = cart.find(
+      (element) => element.name === currentProduct.name
+    );
+
+    if (foundProduct === undefined) {
+      cart.push({
+        name: currentProduct.name,
+        price: currentProduct.price,
+        quantity: 1,
+      });
+    } else {
+      foundProduct.quantity = foundProduct.quantity + 1;
+    }
+
+    printCart();
+    calculateAndUpdatePrice();
+  });
+
   // metemos el dentro del product item
   productItemEl.appendChild(productNameEl);
   productItemEl.appendChild(productButtonEl);
@@ -54,22 +73,22 @@ for (let i = 0; i < products.length; i++) {
   productsListEl.appendChild(productItemEl);
 }
 
-const cart = [
-  {
-    name: "Zanahorias",
-    price: 2,
-    quantity: 3,
-  },
-  {
-    name: "Tomates",
-    price: 0.5,
-    quantity: 1,
-  },
-  {
-    name: "Huevos",
-    price: 1,
-    quantity: 5,
-  },
+let cart = [
+  // {
+  //   name: "Zanahorias",
+  //   price: 2,
+  //   quantity: 3,
+  // },
+  // {
+  //   name: "Tomates",
+  //   price: 0.5,
+  //   quantity: 1,
+  // },
+  // {
+  //   name: "Huevos",
+  //   price: 1,
+  //   quantity: 5,
+  // },
 ];
 
 function calculateAndUpdatePrice() {
@@ -86,56 +105,65 @@ function calculateAndUpdatePrice() {
   priceAmountEl.innerHTML = `${totalPrice} euros`;
 }
 
-const cartListEl = document.querySelector(".cart");
+function printCart() {
+  const cartListEl = document.querySelector(".cart");
+  cartListEl.innerHTML = "";
 
-for (let i = 0; i < cart.length; i++) {
-  const currentItem = cart[i];
+  for (let i = 0; i < cart.length; i++) {
+    const currentItem = cart[i];
 
-  const itemCart = document.createElement("li");
-  itemCart.classList.add("cart__item");
+    const itemCart = document.createElement("li");
+    itemCart.classList.add("cart__item");
 
-  const itemName = document.createElement("p");
-  itemName.classList.add("item__name");
-  itemName.innerHTML = currentItem.name;
+    const itemName = document.createElement("p");
+    itemName.classList.add("item__name");
+    itemName.innerHTML = currentItem.name;
 
-  itemCart.appendChild(itemName);
+    itemCart.appendChild(itemName);
 
-  const itemQuantity = document.createElement("p");
-  itemQuantity.classList.add("item__quantity");
-  itemQuantity.innerHTML = currentItem.quantity;
-
-  itemCart.appendChild(itemQuantity);
-
-  const buttonItem = document.createElement("div");
-  buttonItem.classList.add("item__buttons");
-
-  itemCart.appendChild(buttonItem);
-
-  const itemButtonLess = document.createElement("button");
-  itemButtonLess.classList.add("item__button");
-  itemButtonLess.innerHTML = "-1";
-
-  itemButtonLess.addEventListener("click", () => {
-    currentItem.quantity = currentItem.quantity - 1;
+    const itemQuantity = document.createElement("p");
+    itemQuantity.classList.add("item__quantity");
     itemQuantity.innerHTML = currentItem.quantity;
-    calculateAndUpdatePrice();
-  });
 
-  buttonItem.appendChild(itemButtonLess);
+    itemCart.appendChild(itemQuantity);
 
-  const itemButtonPlus = document.createElement("button");
-  itemButtonPlus.classList.add("item__button");
-  itemButtonPlus.innerHTML = "+1";
+    const buttonItem = document.createElement("div");
+    buttonItem.classList.add("item__buttons");
 
-  itemButtonPlus.addEventListener("click", () => {
-    currentItem.quantity = currentItem.quantity + 1;
-    itemQuantity.innerHTML = currentItem.quantity;
-    calculateAndUpdatePrice();
-  });
+    itemCart.appendChild(buttonItem);
 
-  buttonItem.appendChild(itemButtonPlus);
+    const itemButtonLess = document.createElement("button");
+    itemButtonLess.classList.add("item__button");
+    itemButtonLess.innerHTML = "-1";
 
-  cartListEl.appendChild(itemCart);
+    itemButtonLess.addEventListener("click", () => {
+      currentItem.quantity = currentItem.quantity - 1;
+      itemQuantity.innerHTML = currentItem.quantity;
+      calculateAndUpdatePrice();
+
+      if (currentItem.quantity === 0) {
+        cart = cart.filter((element) => element.name !== currentItem.name);
+        printCart();
+      }
+    });
+
+    buttonItem.appendChild(itemButtonLess);
+
+    const itemButtonPlus = document.createElement("button");
+    itemButtonPlus.classList.add("item__button");
+    itemButtonPlus.innerHTML = "+1";
+
+    itemButtonPlus.addEventListener("click", () => {
+      currentItem.quantity = currentItem.quantity + 1;
+      itemQuantity.innerHTML = currentItem.quantity;
+      calculateAndUpdatePrice();
+    });
+
+    buttonItem.appendChild(itemButtonPlus);
+
+    cartListEl.appendChild(itemCart);
+  }
 }
 
+printCart();
 calculateAndUpdatePrice();
